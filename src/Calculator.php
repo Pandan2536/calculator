e6d274c769d1532d38ceafe188f61c03249be694
Pandan2536 <kanthreeras57@email.nu.ac.th>
2017-05-29 15:42:37 +0700

    first commit

---
+++ src/Calculator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pandan2536\Calculator;
+class Calculator
+{
+     public function add($first_num, $second_num)
+    {
+        return $first_num + $second_num;
+    }
+    public function subtract($first_num, $second_num)
+    {
+        return $first_num - $second_num;
+    }
+    public function multiply($first_num, $second_num)
+    {
+        return $first_num * $second_num;
+    }    
+    public function divide($first_num, $second_num)
+    {
+        return $first_num / $second_num;
+    }
+}
